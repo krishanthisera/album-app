fa89065e10f82177cefb06dd3ce8ad5338387f0e
retnodamayanti <mayahere5@gmail.com>
2023-09-07 19:34:59 +0930

    fix indentation issue

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	_ "github.com/krishanthisera/gitops-for-devs/cmd/api/docs"
 	"github.com/krishanthisera/gitops-for-devs/pkg/routes"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-    "github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 )
 
 // @title Album API
@@ -24,15 +24,15 @@ import (
 // @BasePath  /api/v1
 func main() {
 	router := gin.Default()
-	
+
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"}
-    config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
-    config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
-    router.Use(cors.New(config))
-	
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	router.Use(cors.New(config))
+
 	routes.SetupRoutes(router)
-	
+
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run("localhost:8080")
